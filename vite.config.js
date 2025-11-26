ca88a25878fa1c165a39c2726b2bb196e853e3d1
微信公众号项目 <example@example.com>
2025-11-26 16:42:03 +0800

    更改

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/yourusername.github.io/',
   server: {
     port: 3000,
     host: true
